dd7303adc339788d3b65e4018d36561a0d5501b1
sota-mitsumori <sota.mitsumori@gmail.com>
2025-01-31 22:18:56 +0900

    name-logo added

--- assets/assets.js
+++ assets/assets.js
@@ -12,6 +12,7 @@ import java from './java-logo.png';
 import git from './git.png';
 import right_arrow_white from './right-arrow-white.png';
 import logo from './logo.png';
+import logo_dark from './logo_dark.png';
 import mail_icon from './mail_icon.png';
 import mail_icon_dark from './mail_icon_dark.png';
 import profile_img from './profile-img.png';
@@ -48,6 +49,7 @@ export const assets = {
     git,
     right_arrow_white,
     logo,
+    logo_dark,
     mail_icon,
     mail_icon_dark,
     profile_img,
